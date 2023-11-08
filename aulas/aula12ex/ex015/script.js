5b107d8f67bf8263731f399f1e8abfd8af7156c4
Ryan de Souza Silva <107144128+RyanSS27@users.noreply.github.com>
2023-11-08 20:52:50 -0300

    exercício

--- aulas/aula12ex/ex015/script.js
+++ aulas/aula12ex/ex015/script.js
@@ -1,10 +1,11 @@
-var sexo 
+var sexo
+
 function masculino() {
-    sexo = 'Um homem'
+    sexo = 'homem'
 }
 
 function feminino() {
-    sexo = 'Uma mulher'
+    sexo = 'mulher'
 }
 
 function clicou() {
@@ -12,6 +13,38 @@ function clicou() {
     var data = new Date()
     var ano = data.getFullYear()
     var msg = document.querySelector('div#msg')
+    var img = document.querySelector('div#bolinha').style
+    var idade = ano - Number(nasc.value)
 
-    msg.innerHTML = `${sexo} de ${ano - Number(nasc.value)}`
+    if (idade < 0 || idade > 125) {
+        window.alert('[Erro na data digitada] Verifique os dados e tente novamente.')
+    } else {
+        img.display = 'block'
+        switch (sexo) {
+            case 'homem':
+                msg.innerHTML = `Um homem de ${idade} anos de idade.`
+                if (idade < 11) {
+                    img.backgroundImage = 'url(imagens/menino.jpg)'
+                } else if (idade >= 11 && idade < 18) {
+                    img.backgroundImage = 'url(imagens/jovemh.jpg'
+                } else if (idade >= 18 && idade < 60) {
+                    img.backgroundImage = 'url(imagens/homem.jpg)'
+                } else if (idade > 59) {
+                    img.backgroundImage = 'url(imagens/velho.jpg)'
+                }
+
+                break
+            case 'mulher':
+                msg.innerHTML = `Uma mulher de ${idade} anos de idade.`
+                if (idade < 11) {
+                    img.backgroundImage = 'url(imagens/menina.jpg)'
+                } else if (idade > 10 && idade < 18) {
+                    img.backgroundImage = 'url(imagens/jovemm.jpg)'
+                } else if (idade > 17 && idade < 60) {
+                    img.backgroundImage = 'url(imagens/mulher.jpg)'
+                } else if (idade > 59) {
+                    img.backgroundImage = 'url(imagens/velha.jpg)'
+                }
+        }
+    }
 }
